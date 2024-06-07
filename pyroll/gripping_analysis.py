91f1d3a2c952c3104d9400370d8d4c2655929e96
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-06-07 11:37:40 +0200

    Updated root hooks and added plot for bite angle.

--- pyroll/gripping_analysis.py
+++ pyroll/gripping_analysis.py
@@ -1,17 +1,25 @@
-import shapely
+import sys
 import scipy
+import shapely
 import numpy as np
-from pyroll.core import RollPass
+
+from pyroll.core import RollPass, PassSequence, Unit
 from pyroll.core.hooks import Hook, root_hooks
 
-VERSION = "2.0.1"
-GRIPPING_ELEMENT_COUNT = 11
+VERSION = "2.0.2"
+GRIPPING_ELEMENT_COUNT = 15
 
 RollPass.gripping_elements = Hook[np.ndarray]()
 """Array of z-coordinates of the gripping elements centers from core to side."""
 
 RollPass.gripping_elements_heights = Hook[np.ndarray]()
 """Heights of the gripping elements."""
+
+RollPass.bite_angles = Hook[np.ndarray]()
+"""Angles of bite."""
+
+RollPass.mean_bite_angle = Hook[float]()
+"""Mean bite angle of the roll pass."""
 
 RollPass.passed_gripping_condition = Hook[bool]()
 """Array of entry points of the gripping elements."""
@@ -40,8 +48,8 @@ def gripping_elements_heights(self: RollPass):
     return element_heights
 
 
-@RollPass.passed_gripping_condition
-def passed_gripping_condition(self: RollPass):
+@RollPass.bite_angles
+def bite_angles(self: RollPass):
     if not self.roll.has_set_or_cached("contact_length"):
         return None
 
@@ -57,12 +65,22 @@ def passed_gripping_condition(self: RollPass):
         [self.roll.max_radius - self.roll.surface_interpolation(0, center) for center in self.gripping_elements],
         axis=0).flatten()
 
-    entry_angles = [-np.arcsin(entry_point / local_radius) for entry_point, local_radius in
-                    zip(entry_points, local_roll_radii)]
+    bite_angles = [-np.arcsin(entry_point / local_radius) for entry_point, local_radius in
+                   zip(entry_points, local_roll_radii)]
 
+    return bite_angles
+
+
+@RollPass.mean_bite_angle
+def mean_bite_angle(self: RollPass):
+    return np.mean(self.bite_angles)
+
+
+@RollPass.passed_gripping_condition
+def passed_gripping_condition(self: RollPass):
     results = []
-    for entry_angle in entry_angles:
-        if np.tan(entry_angle) > self.coulomb_friction_coefficient:
+    for bite_angle in self.bite_angles:
+        if np.tan(bite_angle) > self.coulomb_friction_coefficient:
             results.append(True)
         else:
             results.append(False)
@@ -74,3 +92,38 @@ def passed_gripping_condition(self: RollPass):
 
 
 root_hooks.add(RollPass.passed_gripping_condition)
+root_hooks.add(RollPass.bite_angles)
+root_hooks.add(RollPass.mean_bite_angle)
+
+try:
+    from pyroll.report.pluggy import hookimpl, plugin_manager
+    from pyroll.report.utils import create_sequence_plot
+
+
+    @hookimpl(specname="unit_plot")
+    def mean_bite_angles_plot(unit: Unit):
+        if isinstance(unit, PassSequence):
+            if any(isinstance(subunit, RollPass) for subunit in unit):
+                fig, ax = create_sequence_plot(unit)
+                ax.set_ylabel(r"Mean Bite Angle $\alpha_0$")
+                ax.set_title("Mean Bite Angle")
+
+                units = list(unit)
+                if len(units) > 0:
+                    x, y = np.transpose(
+                        [
+                            (index, np.rad2deg(unit.mean_bite_angle))
+                            for index, unit in enumerate(units)
+                            if isinstance(unit, RollPass)
+                        ]
+                    )
+
+                ax.bar(x=x, height=y, width=0.8)
+
+                return fig
+
+
+    plugin_manager.register(sys.modules[__name__])
+
+except ImportError:
+    pass  # report not installed
--- tests/test_solve.py
+++ tests/test_solve.py
@@ -17,7 +17,7 @@ def test_solve(tmp_path: Path, caplog):
         material=["C45", "steel"],
         flow_stress=100e6,
         density=7.5e3,
-        thermal_capacity=690,
+        specific_heat_capacity=690,
     )
 
     sequence = PassSequence(
@@ -66,14 +66,12 @@ def test_solve(tmp_path: Path, caplog):
         print(caplog.text)
 
     try:
-        import pyroll.report
+        from pyroll.report import report
 
-        report = pyroll.report.report(sequence)
-
-        report_file = tmp_path / "report.html"
-        report_file.write_text(report)
-        print(report_file)
-        webbrowser.open(report_file.as_uri())
+        report = report(sequence)
+        f = tmp_path / "report.html"
+        f.write_text(report)
+        webbrowser.open(f.as_uri())
 
     except ImportError:
         pass
